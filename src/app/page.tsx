d906b1bf0f44641e890dd87dcd92bc9bdf1353b1
Dao Manh Tung <daomanhtung4102003@gmail.com>
2025-12-25 02:30:48 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-spring-bg">
+      <h1 className="text-4xl font-bold text-spring-text">
+        Hello World
+      </h1>
+    </main>
+  );
+}
